ec7d9d11233f904669e0b7f9005d505ebd435b7d
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-06-29 15:47:42 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const bcrypt = require('bcryptjs');
+const File = require('./models/File');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost/fileSharing');
+
+const upload = multer({ dest: 'uploads/' });
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.post('/upload', upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname
+  }
+  if (req.body.password != null && req.body.password != '') {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+  const file = await File.create(fileData);
+
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
+});
+
+app.get('/file/:id', handleDownload);
+app.post('/file/:id', handleDownload);
+
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id);
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("password");
+      return
+    }
+
+    if (!await bcrypt.compare(req.body.password, file.password)) {
+      res.render("password", { error: true });
+      return
+    }
+
+  }
+
+  file.downloadCount++;
+  await file.save();
+
+  res.download(file.path, file.originalName);
+}
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
